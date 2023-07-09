34b521093b4d7d3421fffa3d18bc8c274415bef2
Mohammed salih <mohammed.salih@stunntech.com>
2023-07-09 20:01:26 +0530

    completed by salih

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,36 +1,39 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import React from 'react'
 import { Link } from "react-router-dom";
 import "./sidebar.css"
 
 const Sidebar = () => {
-  
-    const [food, setFood] = useState([]);
-    const getfood = async () => {
-      await fetch("/food.json").then((data) => {
-        data.json().then((result) => {
-          setFood(result.food_items);
-    
-        });
-      });
-    };
-  
-  
-    useEffect(() => {
-      getfood();
-    }, []);
 
-    const uniqueCategory = food.map((item)=>item.category)
-    
-    // food.filter((product,index,array)=>array.findIndex ((p)=>p.category==food.category)==index)
-    // console.log(uniqueCategory);
-   
-    return (
-      <div>
-        <div className="block">
-          <div className="section">
-            {uniqueCategory.map((a)=><div key={a.id}>
-                {" "}
+  const [food, setFood] = useState([]);
+  const getfood = async () => {
+    await fetch("/food.json").then((data) => {
+      data.json().then((result) => {
+        setFood(result.food_items);
+
+      });
+    });
+  };
+
+
+  useEffect(() => {
+    getfood();
+  }, []);
+
+
+  return (
+    <div>
+      <div className="block">
+        <div className="section">
+          {food
+            .reduce((uniqueCategories, currentCategory) => {
+              if (!uniqueCategories.find(c => c.category === currentCategory.category)) {
+                uniqueCategories.push(currentCategory);
+              }
+              return uniqueCategories;
+            }, [])
+            .map((a) => (
+              <div key={a.id}>
                 <Link
                   style={{
                     textDecoration: "none",
@@ -40,20 +43,17 @@ const Sidebar = () => {
                   to={`viewcatogary/${a.id}`}
                 >
                   {a.category}
-                  
                 </Link>
                 <br />
                 <br />
                 <br />
-               
-              </div>)}
-  
-          </div>
-          <div>
-          </div>
+              </div>
+            ))}
         </div>
+        <div></div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default Sidebar
