2fa00805d5eeced0a169601760ac0a83a43c9528
Mohammed salih <mohammed.salih@stunntech.com>
2023-07-09 19:52:41 +0530

    salih commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,39 +20,40 @@ const Sidebar = () => {
       getfood();
     }, []);
 
-    const uniqueCategory = food.map((item)=>item.category)
-    
-    // food.filter((product,index,array)=>array.findIndex ((p)=>p.category==food.category)==index)
-    // console.log(uniqueCategory);
    
     return (
       <div>
-        <div className="block">
-          <div className="section">
-            {uniqueCategory.map((a)=><div key={a.id}>
-                {" "}
-                <Link
-                  style={{
-                    textDecoration: "none",
-                    color: "#8a8484",
-                    maxWidth: "20px",
-                  }}
-                  to={`viewcatogary/${a.id}`}
-                >
-                  {a.category}
-                  
-                </Link>
-                <br />
-                <br />
-                <br />
-               
-              </div>)}
-  
+  <div className="block">
+    <div className="section">
+      {food
+        .reduce((uniqueCategories, currentCategory) => {
+          if (!uniqueCategories.find(c => c.category === currentCategory.category)) {
+            uniqueCategories.push(currentCategory);
+          }
+          return uniqueCategories;
+        }, [])
+        .map((a) => (
+          <div key={a.id}>
+            <Link
+              style={{
+                textDecoration: "none",
+                color: "#8a8484",
+                maxWidth: "20px",
+              }}
+              to={`viewcatogary/${a.id}`}
+            >
+              {a.category}
+            </Link>
+            <br />
+            <br />
+            <br />
           </div>
-          <div>
-          </div>
-        </div>
-      </div>
+        ))}
+    </div>
+    <div></div>
+  </div>
+</div>
+
     );
   };
 
