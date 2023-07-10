e3e23cc01e397e4b03777122cdfb3c2a20a76fd8
rajahassan <18ncs05@meaec.edu.in>
2023-07-10 22:36:27 +0530

    end

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./header.css"
+import { Link } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
 
 const Header = () => {
@@ -14,10 +15,10 @@ const Header = () => {
         </div>
         <div className="catogaries">
           <div className="veg">
-            <button><img src="https://w7.pngwing.com/pngs/46/17/png-transparent-maple-leaf-leaf-shape-drawing-leafe-hat-leaf-color-thumbnail.png" alt="" />Pure Veg</button>
+          <Link to={'/'}><button ><img src="https://w7.pngwing.com/pngs/46/17/png-transparent-maple-leaf-leaf-shape-drawing-leafe-hat-leaf-color-thumbnail.png" alt="" />Pure Veg</button></Link>  
           </div>
           <div className="best">
-            <button>Bestseller </button>
+          <Link to={'/'}><button>Bestseller </button></Link>   
           </div>
         </div>
     </div>
